ed76a2895b8268b5f2f1fa0dab282ecdcd537ac4
IgorTimonin <mut4nt@yandex.ru>
2022-09-01 14:13:30 +0300

    cookie secure

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -160,7 +160,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: 'none',
-          secure: false,
+          secure: true,
         })
         .status(200)
         .send({ message: 'Успешный вход' });
